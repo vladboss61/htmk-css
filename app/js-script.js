de9012e1fe24265d5f5e7c12666300f13501d01d
Vladyslav Varvashenko <vladyslav.varvashenko@stryker.com>
2022-07-30 14:44:41 +0300

    Update files.

--- app/js-script.js
+++ app/js-script.js
@@ -1,2 +1,48 @@
-console.log("Hello JavaScript");
-console.log("Hello JavaScript");
+class User {
+    name
+    age
+    
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    get myName() {
+        return this.name
+    }
+
+    set myName(name) {
+        this.name = name
+    }
+
+    print() {
+        console.log('print => name ' + this.name + ' age ' + this.age);
+        console.log(`print => name ${this.name} age ${this.age}`);
+    }
+}
+
+const user2 = {
+    name: "Vlad",
+    age: 24,
+    oncePrint() {
+        console.log('oncePrint => name ' + this.name + ' age ' + this.age);
+        console.log(`oncePrint => name ${this.name} age ${this.age}`);
+    }
+}
+
+
+// let user = new User("Vlad", 24);
+// console.log(user.myName);
+
+// const objSpecial = {
+//     name: "Vlad_123"
+// }
+
+// user2.oncePrint();
+
+// objSpecial.oncePrint = user2.oncePrint;
+
+// objSpecial.oncePrint();
+
+// user.print();
+// user2.oncePrint();
